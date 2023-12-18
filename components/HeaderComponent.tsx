406ea2b428b281f23b9499c2b300cb1f2f03cf19
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2023-12-18 16:48:10 +0100

    feature: add modal

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -2,18 +2,45 @@
 import Link from "next/link";
 import Image from "next/image";
 import classNames from "classnames";
+import Modal from "react-modal";
 import Logo from "@/images/header/logo.svg";
 import s from "./HeaderComponent.module.scss";
 import Search from "./../images/header/search.svg";
 import React, { useState, useEffect } from "react";
 import BurgerMenuOpen from "./../images/header/burger-menu.svg";
+import BurgermenuClose from "./../images/header/BurgerMenuClose.svg";
 
 const menuItem = [
   { title: "Home", type: "link", link: "#" },
-  { title: "Favourite", type: "link", link: "#" },
+  { title: "Favorite", type: "link", link: "#" }
 ];
 
+const favoriteItem = [
+  { title: "Favorite cocktails", link: "#"},
+  { title: "Favorite ingredients", link: "#"}
+]
+
+const useModal = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return {
+    modalIsOpen,
+    openModal,
+    closeModal,
+  };
+};
+
 const HeaderComponent = () => {
+  const { modalIsOpen, openModal, closeModal } = useModal();
+
   const NavigationContent = (
     <ul className={s.header__nav__list}>
       {menuItem.map((item) => (
@@ -22,6 +49,12 @@ const HeaderComponent = () => {
         </li>
       ))}
     </ul>
+  );
+  const ModalContent = (
+    <>
+      {NavigationContent}
+
+    </>
   );
 
   return (
@@ -57,7 +90,7 @@ const HeaderComponent = () => {
           <button className={s.header__thema__button}></button>
           <p className={s.header__thema__dark}>Dark</p>
         </div>
-        <button className={s.menu__open} type="button">
+        <button className={s.menu__open} type="button" onClick={openModal} >
           <Image
             className={s.menu__open__img}
             src={BurgerMenuOpen}
@@ -65,7 +98,45 @@ const HeaderComponent = () => {
           />
         </button>
       </div>
+
+      <Modal
+        className={s.modal}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+        <div className={s.modal__container}>
+          {ModalContent }
+          <form className={s.modal__search} action="">
+          <button className={s.modal__search__button}>
+            <Image
+              className={s.modal__search__svg}
+              src={Search}
+              alt="Search"
+            />
+            <div className={s.modal__search__line}></div>
+            <input
+              className={s.modal__search__input}
+              type="Search |"
+              autoComplete="off"
+              name="searchQuery"
+              placeholder="Search |"
+            />
+          </button>
+        </form>
+
+        <div className={s.modal__thema}>
+          <p className={s.modal__thema__light}>Light</p>
+          <button className={s.modal__thema__button}></button>
+          <p className={s.modal__thema__dark}>Dark</p>
+        </div>
+          </div>
+        <button className={s.modal__close} onClick={closeModal}>
+          <Image src={BurgermenuClose} alt="close" width={20} height={20} />
+
+        </button>
+      </Modal>
     </header>
   );
+
 };
 export default HeaderComponent;
