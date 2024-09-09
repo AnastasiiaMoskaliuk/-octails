aa6ca066a38c8d263f40221ea98955981e1f5fbf
AnastasiiaMoskaliuk <moskaliuk.anastasiia.d@chnu.edu.ua>
2024-09-09 21:08:36 +0200

    fix: update heeader

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,26 +1,32 @@
 "use client";
 import Link from "next/link";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal } from "@mantine/core";
 import ArrowMe from "./../images/header/arrow-up.svg";
 import Image from "next/image";
 import classNames from "classnames";
-import Modal from "react-modal";
 import Logo from "@/images/header/logo.svg";
 import s from "./HeaderComponent.module.scss";
 import Search from "./../images/header/search.svg";
-import React, { useState, useEffect } from "react";
-import BurgerMenuOpen from "./../images/header/burger-menu.svg";
+import React, { useState, useEffect, FC } from "react";
+import BurgerMenu from "./../images/header/burger-menu.svg";
 import BurgermenuClose from "./../images/header/BurgerMenuClose.svg";
+import SearchComponent from "./SearchComponent";
 
-interface NavigationListProps {
-  items: MenuItem[];
-  className: string;
+interface HeaderProps {
+  className?: string;
 }
 
 interface MenuItem {
   title: string;
-  type: string;
   link: string;
-  submenu?: { title: string; link: string }[];
+  type?: string;
+  submenu?: MenuItem[];
+}
+
+interface NavigationListProps {
+  items: MenuItem[];
+  className?: string;
 }
 
 const MenuItem: MenuItem[] = [
@@ -36,207 +42,64 @@ const MenuItem: MenuItem[] = [
   },
 ];
 
-const MenuItemComponent: React.FC<MenuItem> = ({
-  title,
-  type,
-  link,
-  submenu,
-}) => {
-  const [showSubMenu, setShowSubMenu] = useState(false);
-  const [isFavoriteMenuOpen, setFavoriteMenuOpen] = useState(false);
-  const [isArrowRotated, setArrowRotated] = useState(false);
-
-
-  const handleSubMenuShow = () => {
-    setShowSubMenu(true);
-    setArrowRotated(true);
-    setFavoriteMenuOpen(true)
-  };
-
-  const handleSubMenuHide = () => {
-    setShowSubMenu(false);
-    setArrowRotated(false);
-    setFavoriteMenuOpen(false)
-  };
-
-  return (
-    <li
-      key={title}
-      onMouseEnter={type === "submenu" ? handleSubMenuShow : undefined}
-      onMouseLeave={type === "submenu" ? handleSubMenuHide : undefined}
-      style={{
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      {type === "link" && (
-        <Link href={link}>
-          <span>{title}</span>
-        </Link>
-      )}
-      {type === "submenu" && submenu && (
-        <>
-          <span onMouseEnter={handleSubMenuShow}>{title}</span>
-          <ul
-            style={{
-              position: "absolute",
-              top: "100%",
-              left: 0,
-              display: showSubMenu ? "block" : "none",
-            }}
-          >
-            {submenu.map((subitem) => (
-              <li key={subitem.title}>
-                <Link href={subitem.link}>{subitem.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-      {type === "submenu" && (
-        <span    className={classNames(s.arrow, {
-          [s.arrow__rotated]: isArrowRotated && isFavoriteMenuOpen,
-        })}>
-          <Image src={ArrowMe} alt="⌵" />
-        </span>
-      )}
-    </li>
-  );
-};
-
-const NavigationList: React.FC<NavigationListProps> = ({
-  items,
-  className,
-}) => (
-  <ul className={className}>
-    {items.map((item: MenuItem) => (
-      <MenuItemComponent {...item} key={item.title} />
-    ))}
-  </ul>
-);
-
 const HeaderComponent = () => {
-  const [windowWidth, setWindowWidth] = useState(0);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const HeaderLogo: FC<HeaderProps> = ({ className }) => (
+    <Link href="">
+      <Image src={Logo} alt="logo" />
+    </Link>
+  );
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(prevMode => !prevMode);
-  };
-  const checkWindowWidth = () => {
-    setWindowWidth(window.innerWidth);
-  };
+  const NavigationList: FC<NavigationListProps> = ({ items, className }) => (
+    <ul className={className}>
+      {items.map((item, index) => (
+        <li key={index}>{item.title}</li>
+      ))}
+    </ul>
+  );
 
-  useEffect(() => {
-    checkWindowWidth();
-    window.addEventListener("resize", checkWindowWidth);
-    return () => {
-      window.removeEventListener("resize", checkWindowWidth);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (windowWidth >= 1280) {
-      setModalIsOpen(false);
-    }
-  }, [windowWidth]);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
+  const [mobileMenuOpened, { open: openMobileMenu, close: closeMobileMenu }] =
+    useDisclosure(false);
 
   return (
-    <header className={classNames(s.container, s.header)}>
-      <div className={s.header__info}>
-        <div className={s.header__logo}>
-          <Link className={s.header__logo} href="/app/page.tsx" target="_blank">
-            <Image src={Logo} alt="logo" />
-          </Link>
-        </div>
-        <nav className={s.header__nav}>
-          <NavigationList items={MenuItem} className={s.header__nav__list} />
-        </nav>
-      </div>
-      <div className={s.header__forms}>
-        <form className={s.header__search} action="">
-          <button className={s.header__search__button}>
-            <Image
-              className={s.header__search__svg}
-              src={Search}
-              alt="Search"
-            />
-            <div className={s.header__search__line}></div>
-            <input
-              className={s.header__search__input}
-              type="Search |"
-              autoComplete="off"
-              name="searchQuery"
-              placeholder="Search |"
-            />
-          </button>
-        </form>
-        <div className={s.header__thema}>
-          <p className={s.header__thema__light}>Light</p>
-          <button className={s.header__thema__button}></button>
-          <p className={s.header__thema__dark}>Dark</p>
-        </div>
-        <button className={s.menu__open} type="button" onClick={openModal}>
-          <Image
-            className={s.menu__open__img}
-            src={BurgerMenuOpen}
-            alt="Burger-menu"
-          />
+    <header className="container flex justify-between items-center pt-[20px] ">
+      <HeaderLogo />
+      <nav className="hidden lg:flex">
+        <NavigationList items={MenuItem} className="flex gap-[60px]" />
+      </nav>
+      <div className="flex items-center justify-between ">
+        <SearchComponent className="hidden lg:block" />
+        <button className="lg:hidden" onClick={openMobileMenu} type="button">
+          <Image src={BurgerMenu} alt="Burger Menu" width={24} height={24} />
         </button>
       </div>
 
       <Modal
-        className={s.modal}
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
+        opened={mobileMenuOpened}
+        onClose={() => ""}
+        fullScreen
+        radius={0}
+        classNames={{
+          header: "hidden",
+          overlay: "bg-transparent",
+          content: "h-[100%] lg:hidden",
+        }}
+        transitionProps={{ transition: "scale-x", duration: 200 }}
       >
-        <div className={classNames(s.container, s.modal__container)}>
-          <div className={s.modal__thema}>
-            <p className={s.modal__thema__light}>Light</p>
-            <button className={s.modal__thema__button}></button>
-            <p className={s.modal__thema__dark}>Dark</p>
+        <div className="container flex flex-col gap-[40px] py-[20px] px-0 ">
+          <div className="flex justify-between">
+            <HeaderLogo />
+            <button onClick={closeMobileMenu}>
+              <Image src={BurgermenuClose} alt="Close" width={26} height={26} />
+            </button>
           </div>
 
-          <form className={s.modal__search} action="">
-            <button className={s.modal__search__button}>
-              <Image
-                className={s.modal__search__svg}
-                src={Search}
-                alt="Search"
-              />
-              <div className={s.modal__search__line}></div>
-              <input
-                className={s.modal__search__input}
-                type="Search |"
-                autoComplete="off"
-                name="searchQuery"
-                placeholder="Search |"
-              />
-            </button>
-          </form>
-          <nav className={s.modal__nav}>
-            <NavigationList items={MenuItem} className={s.modal__nav__list} />
-          </nav>
-        </div>
-        <button className={s.modal__close} onClick={closeModal}>
-          <Image
-            className={s.modal__close__img}
-            src={BurgermenuClose}
-            alt="close"
-            width={20}
-            height={20}
+          <SearchComponent className="block lg:hidden" />
+
+          <NavigationList
+            items={MenuItem}
+            className="flex flex-col gap-[15px] text-[28px] font-normal"
           />
-        </button>
+        </div>
       </Modal>
     </header>
   );
---
+++ components/SearchComponent.tsx
@@ -0,0 +1,185 @@
+import React, { FC, useState, useEffect, useRef } from "react";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import Search from "@/images/header/search.svg";
+import Close from "@/images/header/BurgerMenuClose.svg";
+
+interface HeaderProps {
+    className?: string;
+  }
+
+type SearchResult = {
+  products: any[];
+  subTexts: any[];
+};
+
+const SearchComponent: FC<HeaderProps> = ({className}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredResults, setFilteredResults] = useState<SearchResult | null>(
+    null
+  );
+  const [showAll, setShowAll] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setShowAll(false);
+        setModalOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputSearchTerm = event.target.value;
+    const formattedSearchTerm = inputSearchTerm.replace(/^\s+/g, "");
+    setSearchTerm(formattedSearchTerm);
+    setModalOpen(true);
+  };
+
+  const clearSearch = () => {
+    setSearchTerm("");
+    setModalOpen(false);
+  };
+
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
+
+  const { products, subTexts } = filteredResults || {
+    products: [],
+    subTexts: [],
+  };
+
+  const resultsToShow = showAll
+    ? [...products, ...subTexts]
+    : [
+        ...products.slice(0, 4),
+        ...subTexts.slice(0, 8 - Math.min(4, products.length)),
+      ];
+
+  return (
+    <div className={` ${className} relative w-[100%] xl:w-max `}>
+      <form className="relative z-50 flex items-center w-[100%] xl:w-max">
+        <input
+          id="searchInput"
+          className="outline-none w-[100%] border-[1px] border-solid border-[#fe7031] rounded h-[38px] pl-[10px] pr-[60px] xl:w-[400px] xl:h-[42px] 2xl:w-[560px] placeholder-gray-500"
+          type="text"
+          placeholder="Search for products..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        {searchTerm && (
+          <button
+            type="button"
+            onClick={clearSearch}
+            className="absolute right-[9%]  cursor-pointer xl:right-[52px] w-[18px] h-[18px] flex items-center justify-center"
+          >
+            <Image src={Close} alt="Clear" width={18} height={18} />
+          </button>
+        )}
+        <Image
+          className="absolute cursor-text right-[3%] xl:right-[5%] w-[18px] h-[18px]"
+          src={Search}
+          alt="Search"
+        />
+      </form>
+      {modalOpen && (
+        <>
+          <div
+            className="fixed inset-0 bg-[#00000010] bg-opacity-50 z-40 transition-opacity duration-300 ease-in-out"
+            onClick={clearSearch}
+          ></div>
+
+          <div
+            className="grid bg-white shadow-md rounded py-4 absolute w-[100%] xl:w-[100%] z-50 transition-opacity duration-300 ease-in-out"
+            ref={modalRef}
+          >
+            {resultsToShow.length > 0 && (
+              <button
+                className="flex items-center gap-[8px] text-[#3e77aa] ml-[22px] mb-[16px] font-bold w-max relative group transition duration-300 hover:underline"
+                onClick={handleShowAll}
+              >
+                <span>Переглянути більше</span>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute right-[-30px] transition-transform duration-300 transform translate-x-0 group-hover:translate-x-3"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M14.7929 6.79289C15.1834 6.40237 15.8166 6.40237 16.2071 6.79289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L16.2071 17.2071C15.8166 17.5976 15.1834 17.5976 14.7929 17.2071C14.4024 16.8166 14.4024 16.18342 14.7929 15.7929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L14.7929 8.20711C14.4024 7.81658 14.4024 7.18342 14.7929 6.79289Z"
+                    fill="#3e77aa"
+                  />
+                </svg>
+              </button>
+            )}
+            {resultsToShow.length > 0 ? (
+              resultsToShow.map((result, index) => (
+                <Link
+                  // href={result.href || ""}
+                  href={`/${result.displayName}`}
+                  key={result._id || `${result.name}-${index}`}
+                  passHref
+                >
+                  <div className="flex items-center bg-white rounded px-4 py-2 hover:bg-[#E5E8ED75] transition-colors duration-300 hover:underline">
+                    {/* {result.image_id ? (
+                      <Image
+                        src={
+                          testProduct.images.find(
+                            (img: any) => img.id === result.image_id
+                          )?.src || ""
+                        }
+                        alt={result.title}
+                        width={50}
+                        height={50}
+                        className="rounded"
+                      />
+                    ) : null} */}
+                    <div className="ml-4">
+                      <p
+                        className={`font-bold ${
+                          result.title
+                            ? "text-[14px] xl:text-[16px]"
+                            : "text-[14px]"
+                        }`}
+                      >
+                        {result.title || result.name}
+                      </p>
+                      {result.price && (
+                        <p className="text-gray-600 text-[14px] xl:text-[16px]">
+                          {result.price}$
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <p className="text-[14px] text-gray-600 px-[22px] w-[100%]">
+                По вашому запиту нічого не знайдено. Уточніть свій запит
+              </p>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SearchComponent;
