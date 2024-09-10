881bcd76b6d6d993f05f886a69e6151a21076a89
AnastasiiaMoskaliuk <moskaliuk.anastasiia.d@chnu.edu.ua>
2024-09-10 09:30:15 +0200

    fix: update footer

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,85 +1,91 @@
+import Link from "next/link";
 import Image from "next/image";
-import classNames from "classnames";
-import s from "./FooterComponent.module.scss";
+import React, { FC } from "react";
+
 import Twitter from "./../images/footer/twitter.svg";
 import Facebook from "./../images/footer/facebook.svg";
 import LinkedIn from "./../images/footer/linkedin.svg";
 import Instagram from "./../images/footer/instagram.svg";
 import LogoFooter from "./../images/footer/LogoFooter.svg";
 
+import ButtonComponent from "./ButtonComponent";
+
+interface FooterProps {
+  className?: string;
+}
+
 const FooterComponent = () => {
+  const FooterLogo: FC<FooterProps> = ({ className }) => (
+    <Link href="">
+      <Image className="pb-[10px]" src={LogoFooter} alt="logo" />
+    </Link>
+  );
+
+  const ContactInfo: FC<FooterProps> = ({ className }) => (
+    <div className="flex flex-col text-[#ffffff] ">
+      <Link
+        href="https://www.google.com/maps/place/Shepherd's+Pub+Zwickau/@50.7180707,12.3743547,12z/data=!4m10!1m2!2m1!1scocktail!3m6!1s0x47a72c89112b837b:0x3ab75332fbcd1572!8m2!3d50.7180707!4d12.4567522!15sCghjb2NrdGFpbFoKIghjb2NrdGFpbJIBA2JhcuABAA!16s%2Fg%2F1pzx_kcbt?entry=ttu"
+        target="_blank"
+      >
+        08060 Zwickau Marienthaler Str. 98,
+      </Link>
+      <Link href="mailto: info@coctails.com">info@coctails.com</Link>
+
+      <Link href="tel:+380961111111">+38 096 111 11 11</Link>
+    </div>
+  );
+
+  const LinkNetworks: FC<FooterProps> = ({ className }) => (
+    <div className="flex flex-col items-center lg:items-start">
+      <h2 className="text-[#ffffff] text-[14px] font-bold uppercase mt-[20px] mb-[20px] lg:text-[18px]">
+        Приєднуйтесь
+      </h2>
+      <div className="flex gap-[10px]">
+        <Link
+          href=""
+          className="flex justify-center items-center w-[50px] h-[50px] bg-[rgba(255,255,255,0.1)] rounded-full hover:bg-[#fd0303] hover:scale-110 transition-transform"
+        >
+          <Image src={Instagram} alt="Instagram" />
+        </Link>
+        <Link
+          href=""
+          className="flex justify-center items-center w-[50px] h-[50px] bg-[rgba(255,255,255,0.1)] rounded-full hover:bg-[#fd0303] hover:scale-110 transition-transform"
+        >
+          <Image src={Twitter} alt="Twitter" />
+        </Link>
+        <Link
+          href=""
+          className="flex justify-center items-center w-[50px] h-[50px] bg-[rgba(255,255,255,0.1)] rounded-full hover:bg-[#fd0303] hover:scale-110 transition-transform"
+        >
+          <Image src={Facebook} alt="Facrbook" />
+        </Link>
+        <Link
+          href=""
+          className="flex justify-center items-center w-[50px] h-[50px] bg-[rgba(255,255,255,0.1)] rounded-full hover:bg-[#fd0303] hover:scale-110 transition-transform"
+        >
+          <Image src={LinkedIn} alt="LinkedIn" />
+        </Link>
+      </div>
+    </div>
+  );
+
   return (
-    <footer className={s.footer}>
-      <div className={classNames(s.container, s.footer__container)}>
-        <div className={s.footer__adress__container}>
-          <a href="#">
-            <Image className={s.footer__logo} src={LogoFooter} alt="Logo" />
-          </a>
-          <ul>
-            <li>
-              <a
-                className={s.footer__adress}
-                href="https://www.google.com/maps/place/Shepherd's+Pub+Zwickau/@50.7180707,12.3743547,12z/data=!4m10!1m2!2m1!1scocktail!3m6!1s0x47a72c89112b837b:0x3ab75332fbcd1572!8m2!3d50.7180707!4d12.4567522!15sCghjb2NrdGFpbFoKIghjb2NrdGFpbJIBA2JhcuABAA!16s%2Fg%2F1pzx_kcbt?entry=ttu"
-                target="_blank"
-              >
-                08060 Zwickau Marienthaler Str. 98,
-              </a>
-            </li>
-            <li>
-              <a
-                className={s.footer__contacts}
-                href="mailto: info@coctails.com"
-              >
-                info@coctails.com
-              </a>
-            </li>
-            <li>
-              <a className={s.footer__contacts} href="tel:+380961111111">
-                +38 096 111 11 11
-              </a>
-            </li>
-          </ul>
+    <footer className="bg-[#fe7031]">
+      <div className="container flex flex-col items-center py-[60px] gap-[50px] md:flex-row md:items-start md:justify-between">
+        <div>
+          <FooterLogo />
+          <ContactInfo />
         </div>
 
-        <div className={s.footer__list__network}>
-          <h2 className={s.footer__text}>Приєднуйтесь</h2>
-          <div className={s.footer__network}>
-            <a href="" className={s.footer__img}>
-              <Image
-                className={s.footer__img__network}
-                src={Instagram}
-                alt="Instagram"
-              />
-            </a>
-            <a href="" className={s.footer__img}>
-              <Image
-                className={s.footer__img__network}
-                src={Twitter}
-                alt="Twitter"
-              />
-            </a>
-            <a href="" className={s.footer__img}>
-              <Image
-                className={s.footer__img__network}
-                src={Facebook}
-                alt="Facrbook"
-              />
-            </a>
-            <a href="" className={s.footer__img}>
-              <Image
-                className={s.footer__img__network}
-                src={LinkedIn}
-                alt="LinkedIn"
-              />
-            </a>
-          </div>
-        </div>
+        <LinkNetworks />
 
-        <div className={s.footer__email}>
-          <h2 className={s.footer__text}>Підпишись на розсилку</h2>
-          <div className={s.footer__form}>
+        <div className="w-[100%] lg:w-auto">
+          <h2 className="text-center text-[#ffffff] text-[14px] font-bold uppercase mt-[20px] mb-[20px] lg:text-[18px] lg:text-start  ">
+            Підпишись на розсилку
+          </h2>
+          <div className="flex justify-center flex-col items-center md:flex-col md:items-center lg:flex-row lg:flex-nowrap lg:items-start">
             <input
-              className={s.footer__form__input}
+              className="w-[290px] h-[50px] mb-[20px] text-black pl-[15px] text-[rgba(255,255,255,0.6)] border-none outline-none text-[16px] font-[400] rounded-[4px] border-[1px] border-[rgba(255,255,255,0.3)] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.15)] lg:w-[250px] lg:mr-[12px]"
               type="email"
               name="em"
               id="email"
@@ -88,7 +94,11 @@ const FooterComponent = () => {
               autoComplete="off"
               pattern=".*[@]gmail[.]com$"
             />
-            <button className={s.footer__form__btn}>Підписатись</button>
+            <ButtonComponent
+              typeButton="FollowButton"
+              text="Підписатись"
+              type="button"
+            />
           </div>
         </div>
       </div>
